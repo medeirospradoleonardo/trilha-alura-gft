5501df4932c85e3a1bcf18eac02db20f3d651f13
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-06-15 12:52:19 -0300

    Aplicando o pattern Chain of Responsibility

--- loja/src/br/com/alura/loja/TestesDescontos.java
+++ loja/src/br/com/alura/loja/TestesDescontos.java
@@ -8,7 +8,7 @@ import br.com.alura.loja.orcamento.Orcamento;
 public class TestesDescontos {
 
     public static void main(String[] args) {
-        Orcamento primeiro = new Orcamento(new BigDecimal("200"), 4);
+        Orcamento primeiro = new Orcamento(new BigDecimal("200"), 6);
         Orcamento segundo = new Orcamento(new BigDecimal("1000"), 1);
 
         CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
