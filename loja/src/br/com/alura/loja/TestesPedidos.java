ad3cbd786cdfbc6d71593932a6f29a759d8ee056
loed <leonardo.prado@gft.com>
2022-06-17 16:19:56 -0300

    Aplicando o pattern Observer

--- loja/src/br/com/alura/loja/TestesPedidos.java
+++ loja/src/br/com/alura/loja/TestesPedidos.java
@@ -1,18 +1,23 @@
 package br.com.alura.loja;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 public class TestesPedidos {
     public static void main(String[] args) {
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+        String cliente = "Rodrigo";
+        BigDecimal valorOrcamento = new BigDecimal("300");
+        int quantidadeItens = 2;
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        GeraPedidoHandler hanlder = new GeraPedidoHandler(/*Dependências*/);
+        GeraPedidoHandler hanlder = new GeraPedidoHandler(
+            Arrays.asList(new SalvarPedidoNoBancoDeDados(), new EnviarEmailPedido())
+        );
         hanlder.execute(gerador);
     }
 }
--- loja/src/br/com/alura/loja/pedido/acao/SalvarPedidoNoBancoDeDados.java
+++ loja/src/br/com/alura/loja/pedido/acao/SalvarPedidoNoBancoDeDados.java
@@ -2,10 +2,11 @@ package br.com.alura.loja.pedido.acao;
 
 import br.com.alura.loja.pedido.Pedido;
 
-public class SalvarPedidoNoBancoDeDados {
+public class SalvarPedidoNoBancoDeDados implements AcaoAposGerarPedido {
     
-    public void executar(Pedido pedido){
-        System.out.println("Salvando pedido no banco de dados");
+    @Override
+    public void executarAcao(Pedido pedido) {
+        System.out.println("Salvando pedido no banco de dados");  
     }
 
 }
